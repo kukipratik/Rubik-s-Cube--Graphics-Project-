fc238f597b0cd8f823a42cbda46a4fc43b9b56df
Pratik Lama <kukipratik123@gmail.com>
2023-08-13 12:24:42 +0545

    render list of cubes

--- main2.py
+++ main2.py
@@ -11,20 +11,27 @@ class Game(Entity):
                texture='textures/sky0', double_sided=True)  # sky
         EditorCamera()
         camera.world_position = (0, 0, -15)
-        self.model, self.texture = 'models/custom_cube', 'textures/rubik_texture'
+        self.cube_model, self.cube_texture = 'models/custom_cube', 'textures/rubik_texture'
         self.load_game()
         app.run()
         
     def load_game(self):
+        self.create_cube_positions()
+        self.CUBES = [Entity(model=self.cube_model, texture=self.cube_texture, position=pos) for pos in self.SIDE_POSITIONS]
         self.PARENT = Entity(model= 'cube', texture = "textures/sky0")
-        Entity(parent= self.PARENT, model= self.model, texture= self.texture, position = (-1,1,1))
-        self.rot = 0
 
+    def create_cube_positions(self):
+        self.LEFT = {Vec3(-1, y, z) for y in range(-1, 2)
+                     for z in range(-1, 2)}
+        self.BOTTOM = {Vec3(x, -1, z) for x in range(-1, 2)
+                       for z in range(-1, 2)}
+        self.SIDE_POSITIONS = self.LEFT | self.BOTTOM
+        
     def input(self, key):
         if key == "left mouse down":
             print("clicked left")
-            self.rot += 90
-            self.PARENT.animate_rotation_x(self.rot, duration=0.5) 
+        #     self.rot += 90
+        #     self.PARENT.animate_rotation_x(self.rot, duration=0.5) 
     
 if __name__ == '__main__':
     game = Game()
